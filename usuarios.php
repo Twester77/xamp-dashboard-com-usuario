9375b3a15e0c240a8ec7cc63589c2f06a594757b
Leonardo F <87826@unifev.edu.br>
2026-03-10 05:58:02 -0300

    primeiro commit

---
+++ usuarios.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acesso ao Sistema</title>
+
+</head>
+
+<main>
+    <div class="header-content">
+        <h2> Gestão de Usuários </h2>
+        <p> Visualize e gerencie as permissões de usuários do sistema. </p>
+</div>
+<div class="table-responsive">
+
+    <table>
+        <thead>
+            <tr>
+
+                <th> ID</th> 
+                <th> Nome </th>
+                <th> E-mail </th>
+                <th> Acesso </th>
+                <th> Status </th>
+                <th> Acões </th>
+
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td> 01 </td>
+                <td> Leonardo Florindo </td>
+                <td> 87826unifev.edu.br </td>
+                <td> Administrador </td>
+                <td> Ativo </td>
+                <td> Editar </td>
+            </tr>
+
+            <tr>
+                <td> 02 </td>            
+                <td>João Lucas </td>
+                <td> Joaolucas_e_gay@unifev.edu.br</td>
+                <td> Administrador </td>
+                <td> Ativo </td>
+                <td> Editar </td>
+            </tr>
+            
+            
+</tbody>
+
+
+</table>              
+        
+    
+</div>
+
+</main>
+
+<body>
+    
+
+
+</body>
+</html>
