09420db96d39dc098666e0823b6c31a4ed87aad3
Leonardo F <87826@unifev.edu.br>
2026-03-13 08:43:50 -0300

    refatorando a estrutura da tabela de usuários e corrigindo detalhes de estilo

--- usuarios.php
+++ usuarios.php
@@ -3,30 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>Acesso ao Sistema</title>    
-      <link rel= "stylesheet" href="dash.css">
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+    <title>Acesso ao Sistema</title>
 
 </head>
 
 <main>
     <div class="header-content">
-        <p> <h2> Gestão de Usuários </h2> </p>
+        <h2> Gestão de Usuários </h2>
         <p> Visualize e gerencie as permissões de usuários do sistema. </p>
 </div>
 <div class="table-responsive">
 
-    <table> 
+    <table>
         <thead>
             <tr>
-                <th> ID </th> 
+                <th> ID</th> 
                 <th> Nome </th>
                 <th> E-mail </th>
                 <th> Acesso </th>
                 <th> Status </th>
                 <th> Acões </th>
-
             </tr>
         </thead>
 
@@ -34,22 +30,28 @@
             <tr>
                 <td> 01 </td>
                 <td> Leonardo Florindo </td>
-                <td> 87826unifev.edu.br </td>
+                <td> 87826@unifev.edu.br </td>
                 <td> Administrador </td>
-                <td> <span class="badge ativo"> Ativo </span> </td>
-                <td> <button class="btn-icon"> <i class="fa-solid fa-pen-to-square fa-lg" style="color: rgb(255, 212, 59);"></i>
-                <td> <button class="btn-danger"> <i class="fa-solid fa-trash-can fa-sm" style="color: rgb(242, 81, 41);"></i>
+                <td> Ativo </td>
+                <td> Editar </td>
+               <td> <a href="#"class="btn-acao"> Editar </a>
+                <a href="#"class="btn-acao"> Excluir </a>
+               </td>
+
+            </tr>
 
             <tr>
                 <td> 02 </td>            
                 <td>João Lucas </td>
-                <td> Joaolucas_e_gay@unifev.edu.br</td>
+                <td> Joaolucas_eh_gay@unifev.edu.br</td>
                 <td> Administrador </td>
-                <td> <span class="badge ativo"> Ativo </span> </td>
-                <td> <button class="btn-icon"> <i class="fa-solid fa-pen-to-square fa-lg" style="color: rgb(255, 212, 59);"></i>
-                <td> <button class="btn-danger"> <i class="fa-solid fa-trash-can fa-sm" style="color: rgb(242, 81, 41);"></i>
-            </tr>
-            
+                <td> Ativo </td>
+                <td> Editar </td>
+                <td>
+                <a href="#"class="btn-acao"> Editar </a>
+                <a href="#"class="btn-acao"> Excluir </a>
+                </td>
+            </tr>           
             
 </tbody>
 
