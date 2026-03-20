dcec2cf6df8e3db14c69b2758e8d9b462a785786
Leonardo F <87826@unifev.edu.br>
2026-03-20 18:53:12 -0300

    adicionado form actions no css e desgrudei os botões do form

--- cad-usuarios.php
+++ cad-usuarios.php
@@ -1,41 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Cadastro de Usuário </title>
+     <link rel= "stylesheet" href="dash.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 </head>
-<section class card="card-form">
+<section class ="card-form">
    <div class= "form header">
-    <h2> <i class= "fa solid fa-user-plus"> Novo Usuário </i>
+    <h2> <i class= "fa solid fa-user-plus"> Novo Usuário </i> </h2>
 <p> Preencha os dados abaixo para registrar um novo usuário.</p>
 
 </div>
 
  <form action= "processa.php" method="POST">
  <div class="form-group">
-    <label for="nome"> Nome Completo > </label>
-    <input type="text" if="nome" name="nome" placeholder ="Ex: Leonardo Rodrigues" required>
+    <label for="nome">  Nome Completo  </label>
+    <input type="text" if="nome" name="nome" placeholder = "Ex: Leonardo Rodrigues" required>
  </div>
 
- <div class= "form group">
+ <div class= "form-group">
     <label for="email"> E-mail > </label>
     <input type= "email" id="email" name="email" placeholder="nome@empresa.com" required>
  </div>
 
  <div class="form-row">
     <div class="form-group flex-1">
-            <label for="senha"> Senha> </label>
+            <label for="senha"> Senha  </label>
             <input type="password" id="senha" name="senha" required>
  </div>
  <div class="form-group flex-1">
     <label for="nivel"> Nivel </label>
     <select id="nivel" name="nivel">
         <option value="1"> Usuario </option>
+        <option value="2"> Administrador </option>
+        <option value="3"> Terceiro </option>
     </select>
- </div>
-
- <div>
+</div>
 
  <div class="form-actions">
     <button type="submit" class="btn-save"> Finalizar Cadastro </button>
@@ -45,6 +49,8 @@
  </div>
 </form>
 <body>
-    
+    <footer>
+    <p> &copy; <?php echo date ('Y'); ?> - Desenvolvido na  aula de Web I - Entre em contato conosco: 0800-7070-6969 </p>
+</footer>
 </body>
 </html>
