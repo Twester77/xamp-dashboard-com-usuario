9bcee9fa0ea00ce92f08bc06feb0a0fba915aad7
Leonardo F <87826@unifev.edu.br~>
2026-03-20 20:10:05 -0300

    adicionado footer menu usuarios e dashboard

---
+++ menu.php
@@ -0,0 +1,14 @@
+<nav>
+            <div class="perfil-usuario">
+                <img src="https:/ui-avatars.com/api/?name=Eric+Freitas&background=008080&color=fff" alt="Avatar">
+                <span> Eric Freitas </span>
+            </div>
+        
+            <ul>
+            <li><a href="#"> Inicio </a> </li>
+            <li> <a href="#"> Projetos </a></li>          
+            <li> <a href="#"> Relatórios </a></li>
+            <li> <a href="#"> Configurações </a></li>
+
+            </ul>
+        </nav>
